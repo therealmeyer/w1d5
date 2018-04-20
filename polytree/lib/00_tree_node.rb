f703d79fe08f10dd713398916defa432e9a96a68
Ryan Meyer <rmeyer@scu.edu>
2018-04-20 11:28:22 -0700

    Add #initialize and #parent= to PolyTreeNode class

--- polytree/lib/00_tree_node.rb
+++ polytree/lib/00_tree_node.rb
@@ -1,3 +1,19 @@
+
 class PolyTreeNode
+
+  attr_reader :parent, :children, :value
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(other_node)
+    @parent = other_node
+    other_node.children << self unless other_node.children.include?(self)
+  end
+
+  
+
 
 end
